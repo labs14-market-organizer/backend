de007d2ed29b3405731cd47333aa821316dc13cc
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-07-10 19:15:50 -0400

    Initializes data/dbConfig.js

---
+++ data/dbConfig.js
@@ -0,0 +1,7 @@
+const knex = require('knex');
+const config = require('../knexfile.js');
+
+const dbEnv = process.env.DB_ENV || 'development';
+
+// Pass proper configuration based on environment
+module.exports = knex(config[dbEnv]);
