c4a339579b21084d3efb0490781e02bd8494ea23
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-07-18 16:30:35 -0400

    Tweaks server.test.js for easier testing

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
 const server = require('./server.js');
+const request = require('supertest')(server);
 
 describe('test environment', () => {
   it('runs', () => {
@@ -10,15 +10,13 @@ describe('test environment', () => {
 describe('server', () => {
   describe('/ GET', () => {
     it('should return 200 status', () => {
-      return request(server)
-        .get('/')
+      return request.get('/')
         .expect(200);
     })
 
     it('should return provided welcome message', () => {
       const expected = {"message": "Hello from CloudStands."};
-      return request(server)
-        .get('/')
+      return request.get('/')
         .then(res => expect(res.body).toEqual(expected));
     })
   })
