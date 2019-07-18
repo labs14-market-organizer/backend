98dad4add2e036edf522bb3272a5682c9cd50139
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-07-18 14:51:02 -0400

    Improves /auth tests & creates tests for /userlist endpoints

--- api/auth/index.test.js
+++ api/auth/index.test.js
@@ -1,19 +1,16 @@
-const request = require('supertest');
 const server = require('../server');
-const db = require('./model');
+const request = require('supertest')(server);
 
-describe('server', () => {
-  describe('/ GET', () => {
+describe('/auth', () => {
+  describe('/google GET', () => {
     it('should return 302 status', () => {
-      return request(server)
-        .get('/auth/google')
+      return request.get('/auth/google')
         .expect(302);
     })
 
     it('should redirect to Google OAuth 2.0', () => {
       const expected = "https://accounts.google.com/o/oauth2/v2/auth";
-      return request(server)
-        .get('/auth/google')
+      return request.get('/auth/google')
         .then(res => expect(res.header.location.split('?')[0]).toBe(expected));
     })
   })
