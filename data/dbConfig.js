e4641673dd3a6d3d383b91c5acfa08fabef2dfd2
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-07-18 13:21:35 -0400

    Removes ternary OR operator in dbConfig.js

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const dbEnv = process.env.DB_ENV || 'development';
+const dbEnv = process.env.DB_ENV;
 
 // Pass proper configuration based on environment
 module.exports = knex(config[dbEnv]);
