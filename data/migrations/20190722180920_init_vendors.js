67be02cd1787e4ae8a78930a886aadfa8d340d89
Lambda_School_Loaner_146 <lambda_school_loaner_146@Lambda-School-Loaner-146s-MacBook-Pro.local>
2019-07-22 18:23:24 -0400

    created vendors migration

---
+++ data/migrations/20190722180920_init_vendors.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("vendors", vendors => {
+      vendors.increments();
+      vendors.integer("admin_id")
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+      vendors.string('name');
+      vendors.text('description');
+      vendors.text('items');
+      vendors.boolean('electricity');
+      vendors.boolean('ventilation');
+      vendors.boolean('loud');
+      vendors.text('other_special');
+      vendors.string('website');
+      vendors.string('facebook');
+      vendors.string('twitter');
+      vendors.string('instagram');
+      vendors.timestamps();
+      
+
+    })
+   }
+   exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("vendors");
+   };
