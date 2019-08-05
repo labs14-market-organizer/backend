0985035055e4bb4ffae0714d53dde76b6f1fa57c
Matt Poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-05 14:00:12 -0400

    Hotfix user GET 404s

--- api/users/index.js
+++ api/users/index.js
@@ -5,8 +5,10 @@ const Users = require("./model");
 router.get('/', protect, (req, res ) => {
     const id = req.user_id;
     Users.findById(id)
-    .then(users => {
-        res.status(200).json(users);
+    .then(user => {
+        !user
+            ? res.status(404).json({message: 'No user with that ID exists in our database'})
+            : res.status(200).json(users);
     })
     .catch(err => {
         res
@@ -17,8 +19,10 @@ router.get('/', protect, (req, res ) => {
 router.get('/:id', (req, res ) => {
     const {id} = req.params;
    Users.findById(id)
-        .then(users => {
-            res.status(200).json(users);
+    .then(user => {
+        !user
+            ? res.status(404).json({message: 'No user with that ID exists in our database'})
+            : res.status(200).json(users);
         })
         .catch(err => {
             res
