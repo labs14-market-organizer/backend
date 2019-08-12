35d8f26f16d799495e576c033333ce38f217fa38
Matt Poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-12 17:14:01 -0400

    Revert "Contacts"

--- api/vendors/validate.js
+++ api/vendors/validate.js
@@ -9,7 +9,7 @@ module.exports = [
     .optional(),
   body('description').isString()
     .withMessage("'description must be a string")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('items').isArray()
     .withMessage("'items' must be an array")
     .optional(),
@@ -18,33 +18,26 @@ module.exports = [
     .optional(),
   body('electricity').isBoolean()
     .withMessage("'electricity must be a boolean value")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('ventilation').isBoolean()
     .withMessage("'ventilation' must be a boolean value")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('loud').isBoolean()
     .withMessage("'loud' must be a boolean value")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('other_special').isString()
     .withMessage("'other_special' must be a string")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('website').isURL()
     .withMessage("'website' must be a URL")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('facebook').isString()
     .withMessage("'facebook' must be a string")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('twitter').isString()
     .withMessage("'twitter' must be a string")
-    .optional({ nullable: true }),
+    .optional({nullable: true}),
   body('instagram').isString()
     .withMessage("'instagram' must be a string")
-    .optional({ nullable: true }),
-  body('email').isEmail()
-    .withMessage("'email' must be a vaild email")
-    .optional(),
-  body('phone').isString()
-    .matches(`^[0-9]{3}-[0-9]{3}-[0-9]{4}$`)
-    .withMessage("'phone' must be in the format '###-###-####'")
-    .optional(),
-]
+    .optional({nullable: true}),
+];
