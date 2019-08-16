28a5f4f99fda4d873a280de897148e940a27001d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-16 18:37:31 -0400

    Removes unnecessary .catch

--- api/markets/model.js
+++ api/markets/model.js
@@ -429,7 +429,6 @@ async function findReserveByDate(marketID, date) {
         .whereIn('mb.id', booths)
         .groupBy('mb.id')
         .orderBy('mb.id')
-        .catch(err => console.error(err));
     return result;
 }
 
