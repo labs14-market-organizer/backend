70280ea0698b11d30d6364aebcbf803b3fe4a86f
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-19 19:03:22 -0400

    Fixes seeded rules to be more generic

--- data/seeds/03_markets.js
+++ data/seeds/03_markets.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex) {
-  const rules = "I. VENDOR ELIGIBILITY INFORMATION & REQUIREMENTS A. Producer-Only Market: • The Lafayette Market is a producer-only market. • All products sold must be grown or otherwise produced by the vendor. No resale is permitted. • A vendor may not sell products grown or produced by others. RULES & REGULATIONS Updated March, 2019 • Vendors warrant the quality of all his/her products when they are offered to the public. • Produce grown or products produced at a location not listed in the application are not eligible to be sold - except by cooperatives, as noted below."
+  const rules = "I. VENDOR ELIGIBILITY INFORMATION & REQUIREMENTS A. Producer-Only Market: • This market is a producer-only market. • All products sold must be grown or otherwise produced by the vendor. No resale is permitted. • A vendor may not sell products grown or produced by others. RULES & REGULATIONS Updated March, 2019 • Vendors warrant the quality of all his/her products when they are offered to the public. • Produce grown or products produced at a location not listed in the application are not eligible to be sold - except by cooperatives, as noted below."
   const markets = [
     {
       admin_id: 1,
