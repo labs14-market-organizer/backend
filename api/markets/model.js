4e2b2f3be581e6a6668a1d3f7f438f41994cc9de
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-19 19:53:49 -0400

    One last fix to findReserveByDate

--- api/markets/model.js
+++ api/markets/model.js
@@ -428,8 +428,7 @@ async function findReserveByDate(marketID, date) {
         )
         .whereIn('mb.id', booths)
         .groupBy('mb.id')
-        .orderBy('mb.id')
-        .catch(err => console.error(err))
+        .orderBy('mb.id');
     return result;
 }
 
