ad66bd56f0f3659d208022234d22c7eb3e9bb721
Matt Poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-19 20:27:04 -0400

    Fixes date on seeds for market_reserve

--- data/seeds/08_market_reserve.js
+++ data/seeds/08_market_reserve.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex) {
   const date1 = "2019-08-24 00:00:00+00";
-  const date2 = "2019-09-25 00:00:00+00";
+  const date2 = "2019-08-25 00:00:00+00";
   const reserve = [
     {
       reserve_date: date1,
