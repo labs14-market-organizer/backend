f49e66c9ceecf451029d62eeca02ff3cfa7cc605
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-26 18:30:18 -0400

    Fixes expiration passed on login

--- api/genToken.js
+++ api/genToken.js
@@ -5,7 +5,7 @@ module.exports = (user, redirect = false) => {
   const { id } = user;
   const payload = { subject: id };
   const jwtSecret = process.env.JWT_SECRET;
-  const exp = 1000*60*60*2; // 2 hours
+  const exp = Date.now() + 1000*60*60*2; // 2 hours
   const opt = { expiresIn: `${exp}ms` };
   const token = jwt.sign(payload, jwtSecret, opt);
   return !!redirect
