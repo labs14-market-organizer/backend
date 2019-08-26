b8331bee34fb0402c0964cd49e56db68be3cc786
Matt Poloni <46501814+matt-poloni@users.noreply.github.com>
2019-08-26 17:17:07 -0400

    Removes leftovers from resolving merge conflict

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -9,11 +9,7 @@ module.exports = (to, subject, html) => {
     subject,
     html
   }
-<<<<<<< HEAD
-  console.log(process.env.NODE_ENV)
-=======
   // Switch to prevent SendGrid from failing tests or sending email
->>>>>>> 927269b6a1d96a235597350e6b331eb74b1c15b4
   return process.env.NODE_ENV !== 'testing'
     ? sgMail.send(msg)
     : null;
